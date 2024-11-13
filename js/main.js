2aaf8a8d3b7f7a4d691104fb615faf936c59a29b
Manjur-WD <mak985812@gmail.com>
2024-11-14 02:54:09 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -2,10 +2,42 @@ const navBtn = document.getElementById("nav-btn");
 const closeNav = document.getElementById("close-nav");
 const navMenu = document.querySelector(".nav-list");
 
-navBtn.addEventListener("click",()=>{
+navBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
     navMenu.classList.add("active");
 })
-closeNav.addEventListener("click",()=>{
+closeNav.addEventListener("click",(e)=>{
+    e.preventDefault();
     navMenu.classList.remove("active");
 })
+
+// LENIS
+
+// Initialize Lenis with smooth scroll options
+const lenis = new Lenis({
+    smooth: true,              // Enables smooth scrolling
+    duration: 1,             // Adjusts scroll duration (higher = slower, smoother scroll)
+    easing: (t) => t * (2 - t) // Ease-in-out effect (you can adjust this easing function for different effects)
+  });
+  
+  // Listen for the scroll event and log the event data
+  lenis.on('scroll', (e) => {
+    console.log(e);
+  });
+  
+  // Use requestAnimationFrame to continuously update the scroll
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf);
+
+//   fancybox
+
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
+  
+  
 
